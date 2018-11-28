b3e32592a2ffe78747ecdc031d375c34dc4c8d7e
Viral Joshi <viraljoshi23@live.com>
2018-11-27 21:58:14 -0600

    Custom AppBar tutorial.

--- App.js
+++ App.js
@@ -1,49 +1,38 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+import {StyleSheet, Text, View} from 'react-native';
+import {BACKGROUND_CLOUDY_COLOR, STATUS_BAR_COLOR} from "./src/helpers/colors";
+import CustomAppStatusBar from './src/helpers/AppBar';
 
 type Props = {};
 export default class App extends Component<Props> {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
-    );
-  }
+    render() {
+        return (
+            <View style={styles.container}>
+                <CustomAppStatusBar
+                    backgroundColor={STATUS_BAR_COLOR}
+                    barStyle="dark-content"
+                />
+
+                <View style={styles.textContainer}>
+                    <Text style={styles.appName}>React Native Fun</Text>
+                </View>
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    container: {
+        flex: 1,
+        backgroundColor: BACKGROUND_CLOUDY_COLOR,
+    },
+    textContainer: {
+        flex: 1,
+        padding: 15,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    appName: {
+        fontSize: 40,
+    }
 });
---
+++ src/helpers/colors.js
@@ -0,0 +1,2 @@
+export const STATUS_BAR_COLOR = "#fafafa";
+export const BACKGROUND_CLOUDY_COLOR = "#f1f1f1";
